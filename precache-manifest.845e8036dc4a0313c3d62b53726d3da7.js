c52f19e7e6205f95795ed763457ed7a2b1f44336
Perry Fustero <pjfust@gmail.com>
2020-01-25 01:33:16 -0500

    Updates

---
+++ precache-manifest.845e8036dc4a0313c3d62b53726d3da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f997e892188d8d3ee2ae592f13ba981e",
+    "url": "//perryf.github.io/chord-reader/index.html"
+  },
+  {
+    "revision": "314adf8fbcbc6faaef5b",
+    "url": "//perryf.github.io/chord-reader/static/css/main.6abc8c60.chunk.css"
+  },
+  {
+    "revision": "a6a786c641c01310efa1",
+    "url": "//perryf.github.io/chord-reader/static/js/2.e84ca5b6.chunk.js"
+  },
+  {
+    "revision": "314adf8fbcbc6faaef5b",
+    "url": "//perryf.github.io/chord-reader/static/js/main.2230ef08.chunk.js"
+  },
+  {
+    "revision": "3caa7a630d9e64bbd9ef",
+    "url": "//perryf.github.io/chord-reader/static/js/runtime-main.5907b31c.js"
+  }
+]);
