59a21103ad8fc8114e9da5cdf1887f0bd17c102d
Perry Fustero <pjfust@gmail.com>
2020-02-29 05:22:29 -0500

    Updates

---
+++ precache-manifest.5facca6546d100da66240ca96d6e4119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17753eaac84b30885f08653f573136b8",
+    "url": "//perryf.github.io/chord-reader/index.html"
+  },
+  {
+    "revision": "cb6e752f281a8ec1deb5",
+    "url": "//perryf.github.io/chord-reader/static/css/main.8f018c71.chunk.css"
+  },
+  {
+    "revision": "f077be0f378348c94303",
+    "url": "//perryf.github.io/chord-reader/static/js/2.744db1e1.chunk.js"
+  },
+  {
+    "revision": "cb6e752f281a8ec1deb5",
+    "url": "//perryf.github.io/chord-reader/static/js/main.0ec87645.chunk.js"
+  },
+  {
+    "revision": "3caa7a630d9e64bbd9ef",
+    "url": "//perryf.github.io/chord-reader/static/js/runtime-main.5907b31c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//perryf.github.io/chord-reader/precache-manifest.89550c18dde98c9bbc388e5c204506e2.js"
+  "//perryf.github.io/chord-reader/precache-manifest.5facca6546d100da66240ca96d6e4119.js"
 );
 
 self.addEventListener('message', (event) => {
